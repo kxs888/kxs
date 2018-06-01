9f4dce1721fd5aea683d093bd1bc17b01adc6ad0
kxs888 <272703895@qq.com>
2018-06-02 01:05:52 +0800

    fe

--- app/api/controller/Index.php
+++ app/api/controller/Index.php
@@ -2,7 +2,7 @@
 namespace app\api\controller;
 use app\common\controller\Common;
 use app\api\model\Member;
-use app\api\Validate;
+// use app\api\Validate;
 // include '../validate/Validate.php';
 class Index extends Common
 {
@@ -43,7 +43,38 @@ class Index extends Common
         
         
     }
-        
+    public function isPhone($phone){
+        if(strlen($phone) == 11){
+            $n = preg_match("/13[123569]{1}\d{8}|15[1235689]\d{8}|188\d{8}/",$phone);
+            if($n != 0){
+                return true;
+            }
+        } else{
+            return false;
+        }
+    
+        function isUsername($username){
+            //中英文6-20个字符正则
+            $preg='/^[a-zA-Z\x{4e00}-\x{9fa5}]{6,20}$/u';
+            $n = preg_match($preg,$username);
+            if($n != 0){
+                return true;
+            } else {
+                return false;
+            }
+    
+            function isPWD($passwd){
+                $n = preg_match('/^([a-zA-Z0-9]){6,16}$/', $password);
+                if($n != 0){
+                    return true;
+                } else {
+                    return false;
+                }
+    
+            }
+        }
+    }
+}  
         
         
         
