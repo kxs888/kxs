038bc08dec95c1a0e146d0227dd5198fa0a5aed6
kxs888 <272703895@qq.com>
2018-06-07 21:57:10 +0800

    ,m,m,

--- app/api/controller/Image.php
+++ app/api/controller/Image.php
@@ -10,7 +10,7 @@ class Image{
         // 图片二  
         $path2 = '../public/static/admin/images/1.jpg';  
         // 创建图片对象  1
-        $image1 = imagecreatefromjpeg($path1);  
+        $image1 = imagecreatefromjpeg($path1);   
         $image2 = imagecreatefromjpeg($path2); 
         // 合成图片  
         imagecopymerge($image1, $image2, 0, 0, 0, 0, imagesx($image2), imagesy($image2), 100);  
