800cd731036181d5d08374be8cb82fe5a89b1a83
kxs888 <272703895@qq.com>
2018-06-07 21:30:52 +0800

    vvv

--- app/api/controller/Image.php
+++ app/api/controller/Image.php
@@ -2,7 +2,7 @@
 namespace app\api\controller;
 class Image{
     
-    public function imageMerge(){
+    public function imageMerge($name){
         
         // 图片一  
         $path1 = '../public/static/admin/images/3.jpg';  
@@ -12,7 +12,7 @@ class Image{
         $image1 = imagecreatefromjpeg($path1);  
         $image2 = imagecreatefromjpeg($path2);  
         // 合成图片  
-        imagecopymerge($image1, $image2, 0, 0, 0, 0, imagesx($image2)/2, imagesy($image2), 100);  
+        imagecopymerge($image1, $image2, 0, 0, 0, 0, imagesx($image2), imagesy($image2), 100);  
         // 输出合成图片  
         // $imagename = rand(1000,9999).'-'.time().'jpg';
         $name = input('post.name');
