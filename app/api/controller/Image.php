c921381e16e9effd0574a56e482bf821d01105ee
kxs888 <272703895@qq.com>
2018-06-07 20:27:44 +0800

    cc

--- app/api/controller/Image.php
+++ app/api/controller/Image.php
@@ -30,7 +30,9 @@ class Image{
     public function textMerge(){
         $name = input('post.name');
         $img = rand(1,5).'.jpg';
-        $path = '../public/static/admin/images/$img';
+        $path = '..public/static/admin/images/$img';
+        echo $path;
+        exit();
         $image = imagecreatefromjpeg($path);
         $black = imagecolorallocate($img, 100, 100,100);//字体颜色 RGB
         $ttf = '../public/static/admin/lib/fangzheng.ttf';
