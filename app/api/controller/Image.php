48dfe89a2df1db53e75d62d75a3fdc72a4afd952
kxs888 <272703895@qq.com>
2018-06-07 18:30:30 +0800

    fff

--- app/api/controller/Image.php
+++ app/api/controller/Image.php
@@ -4,22 +4,25 @@ class Image{
     
     public function getImage(){
         
-// 图片一  
-    $path1 = '../public/static/admin/images/1.jpg';  
-    // 图片二  
-    $path2 = '../public/static/admin/images/3.jpg';  
-    // 创建图片对象  
-    $image1 = imagecreatefromjpeg($path1);  
-    $image2 = imagecreatefromjpeg($path2);  
-    // 合成图片  
-    imagecopymerge($image1, $image2, 0, 0, 0, 0, imagesx($image2)/2, imagesy($image2), 100);  
-    // 输出合成图片  
-    $imagename = rand(1000,9999).'-'.time().'jpg';
-    header('Content-Type:image/jpeg');
-    imagejpeg($image1,'../public/static/admin/images/a.jpg');
+        // 图片一  
+        $path1 = '../public/static/admin/images/3.jpg';  
+        // 图片二  
+        $path2 = '../public/static/admin/images/1.jpg';  
+        // 创建图片对象  
+        $image1 = imagecreatefromjpeg($path1);  
+        $image2 = imagecreatefromjpeg($path2);  
+        // 合成图片  
+        imagecopymerge($image1, $image2, 0, 0, 0, 0, imagesx($image2)/2, imagesy($image2), 100);  
+        // 输出合成图片  
+        $imagename = rand(1000,9999).'-'.time().'jpg';
+        header('Content-Type:image/jpeg');
+        imagejpeg($image1,'../public/static/admin/images/a.jpg');
+        
+        //释放内存
+        imagedestroy($image1);
+        imagedestroy($image2);
+        
+        return "http://kxs.ruohua.club/static/admin/images/a.jpg";
     
-    //释放内存
-    imagedestroy($image1);
-    imagedestroy($image2);
     }
 }
