f9c6c9c930cedcbd7c013092296f355c6560bb83
kxs888 <272703895@qq.com>
2018-06-08 20:41:44 +0800

    ffdf

--- app/api/controller/Index.php
+++ app/api/controller/Index.php
@@ -3,7 +3,7 @@ namespace app\api\controller;
 use app\common\controller\Common;
 use think\Db;
 use app\api\model\User;
-use app\api\Validate\Validate;
+use app\api\Validate;
 
 class Index extends Common
 {
