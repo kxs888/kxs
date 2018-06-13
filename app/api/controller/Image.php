ca649ed360aab1650a3c606530debba1e6952fb6
kxs888 <272703895@qq.com>
2018-06-13 12:35:08 +0800

    aaaa

--- app/api/controller/Image.php
+++ app/api/controller/Image.php
@@ -47,7 +47,7 @@ class Image
         $str = $request->post('str');
         $str = trim($str);
         $date = Request::instance()->post('date');
-        $date = date("Y年m月d日",$date);
+        $time = date("Y年m月d日",$date);
         echo $date;
         exit();
         //随机获取底图
