3de4ff1591cab8815babf68801243d707216eb38
kxs888 <272703895@qq.com>
2018-06-13 11:42:03 +0800

    24

--- app/api/controller/Image.php
+++ app/api/controller/Image.php
@@ -57,8 +57,6 @@ class Image
         $font =  '../public/static/admin/lib/ku.ttf';
         $circle_size = '0';
         $len = strlen($str);
-        echo $len;
-        exit();
         if($len <= 6){
             $font_size = 36;
             imagefttext($image, $font_size, $circle_size, 255, 805, $black1, $font, $str);
