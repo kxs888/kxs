6ac024409320e467645c82e426861f1d857e9882
kxs888 <272703895@qq.com>
2018-06-13 11:15:14 +0800

    afefe

--- app/api/controller/Image.php
+++ app/api/controller/Image.php
@@ -58,15 +58,15 @@ class Image
         $circle_size = '0';
         $len = strlen($str);
         if($len <= 2){
-            $font_size = 40;
+            $font_size = 35;
         } else { 
-            $font_size = 40;
+            $font_size = 32;
         }
          $fontBox1 = imagettfbbox($font_size, 0, $font, $str);
          $fontBox2 = imagettfbbox($font_size, 0, $font, $date);
         //将字体加入图片中
         imagefttext($image, $font_size, $circle_size, 261, 805, $black1, $font, $str);
-        imagefttext($image, 30, $circle_size, 305, 870, $black2, $font, $date);
+        imagefttext($image, 28, $circle_size, 305, 870, $black2, $font, $date);
         $xid = time().rand(1000,9999).'.png';
         header('Content-Type:image/png');
         
