f2b90452e607a43d858c24da170fbcf9566f622b
kxs888 <272703895@qq.com>
2018-06-13 12:37:44 +0800

    fe

--- app/api/controller/Image.php
+++ app/api/controller/Image.php
@@ -46,7 +46,7 @@ class Image
         $request = Request::instance();
         $str = $request->post('str');
         $str = trim($str);
-        $date =$requset->post('date');
+        $date = $request->post('date');
         $time = date("Y年m月d日",$date);
         echo $time;
         exit();
