70c2d526519f0bfdedd5d41a3d9e54b64f0d62d6
kxs888 <272703895@qq.com>
2018-06-15 17:32:33 +0800

    fff

--- app/api/controller/Tong.php
+++ app/api/controller/Tong.php
@@ -10,8 +10,8 @@ class Tong {
        $file_path = "E:/count.txt";
        if(!file_exists($file_path)){
            $one_file=fopen("count.txt","w+"); //建立一个统计文本，如果不存在就创建
-           fwrite("count.txt","1");  //把数字1写入文本
-           fclose("$one_file");
+           fwrite($file_path,"1");  //把数字1写入文本
+           fclose($file_path);
        }else{ //如果不是第一次访问直接读取内容，并+1,写入更新后再显示新的访客数
            $num=file_get_contents($file_path);
            $num++;
